95cb003dd0b4caab3e23f4d3f31e8079d805cc3a
melissasalcedo <salcedomelissa189@gmil.com>
2024-01-08 01:51:19 +0800

    Input numbers

--- biggestnumber.py
+++ biggestnumber.py
@@ -4,7 +4,11 @@
 
 # Ask user for input
 
+number1 = input("What's the first number?: ")
+number2 = input("What's the second number?: ")
+number3 = input("What's the third number?: ")
 # Determine the highest number
+
 # if number1 is the highest
 # if number2 is the highest
 # if number3 is the highest
