f3cc5853026e6dc7d2b41dcd36d3b99ce075ca4f
melissasalcedo <salcedomelissa189@gmil.com>
2024-01-07 18:26:50 +0800

    pseudocode

---
+++ biggestnumber.py
@@ -0,0 +1,12 @@
+# Ask user to input 3 numbers. Detremine what is the highest number
+
+# Pseudocode
+
+# Ask user for input
+
+# Determine the highest number
+# if number1 is the highest
+# if number2 is the highest
+# if number3 is the highest
+
+# else if all equal
