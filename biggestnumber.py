c4b07f5d6213e41953a207fb68f1ca2291eab852
melissasalcedo <salcedomelissa189@gmil.com>
2024-01-08 17:57:07 +0800

    Applying the design

--- biggestnumber.py
+++ biggestnumber.py
@@ -1,19 +1,45 @@
-# Ask user to input 3 numbers. Detremine what is the highest number
-# Pseudocode
+import customtkinter as ctk
+import tkinter as tk
+from tkinter import PhotoImage, Label
+
+# Creating the main window
+
+app = ctk.CTk()
+app.title('What is the 3 numbers?')
+app.geometry("500x250")
+app.config(bg="#E1CFCF")
+app.resizable(False, False)
+
+# Entry widgets
+entry1 = tk.Entry(app, width=10)
+entry2 = tk.Entry(app, width=10)
+entry3 = tk.Entry(app, width=10)
+
+entry1.grid(row=0, column=0, pady=5)
+entry2.grid(row=1, column=0, pady=5)
+entry3.grid(row=2, column=0, pady=5)
 
 # Ask user for input
+def find_the_biggest_number():
+    try:
+        number1 = int(entry1.get())
+        number2 = int(entry2.get())
+        number3 = int(entry3.get())
 
-number1 = int(input("What's the first number?: "))
-number2 = int(input("What's the second number?: "))
-number3 = int(input("What's the third number?: "))
+        # Find the highest number
+        biggest_number = max(number1, number2, number3)
+        
+        # Display the result in window
+        if number1 == number2 == number3:
+            print("There is no highest, All numbers are equal")
+        else:
+            print(f"The biggest number is: {biggest_number}")
+        
+    except ValueError:
+        result_label.config(text="Please enter valid numbers.")
 
-# Find the highest number
-biggest_number = max(number1, number2, number3)
+result_label = tk.Label(app, text="Result will be displayed here.")
 
-# If the numbers are equal
-if number1 == number2 == number3:
-    print("There is no highest, All numbers are equal")
-else:
-    print(f"The biggest number is: {biggest_number}")
+
     
-
+app.mainloop()
