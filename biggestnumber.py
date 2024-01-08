0d720100fa07bbff24b7d6a4d520325e3a3c93a3
melissasalcedo <salcedomelissa189@gmil.com>
2024-01-08 15:23:20 +0800

    Changing my code

--- biggestnumber.py
+++ biggestnumber.py
@@ -1,23 +1,18 @@
 # Ask user to input 3 numbers. Detremine what is the highest number
-
 # Pseudocode
 
 # Ask user for input
 
-number1 = input("What's the first number?: ")
-number2 = input("What's the second number?: ")
-number3 = input("What's the third number?: ")
+number1 = int(input("What's the first number?: "))
+number2 = int(input("What's the second number?: "))
+number3 = int(input("What's the third number?: "))
 
-# Determine the highest number
-# if number1 is the highest
-if number1 >= number2 and number1 >= number3:
-    print(f"The biggest number is: {number1}")
+# Find the highest number
 
-# if number2 is the highest
-if number2 >= number1 and number2 >= number3:
-    print(f"The biggest number is: {number2}")
-# if number3 is the highest
-if number3 >= number2 and number3 >= number1:
-    print(f"The biggest number is: {number3}")
+# If the numbers are equal
 
-# else if all equal
+
+
+
+    
+
